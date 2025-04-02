447f3567b83f9afab43c9b6b4468eef6835da2f0
Liz <lizmarie.mendez86@gmail.com>
2025-04-02 00:23:13 -0400

    express is up, /users created

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,22 @@
-const express= require('express');
+const express =require('express');
 const app = express();
-const port =8081;
+const port = 8011;
+const knex = require('knex')(require('../knexfile.js')["development"]);
+app.get('/', (req,res)=>{
+    res.send('App is up and running')
 
+})
+
+app.listen(port, ()=> {
+    console.log('Knex and Express are green')
+})
+
+app.get('/users', (req,res)=>{
+    knex('users') 
+    .select('*')
+    .then(users => {
+      const userNames = users.map(user => user.first_name);
+      res.json(userNames);
+    })
+
+} )
--- server/src/migrations/20250401225801_create_items_table.js
+++ server/src/migrations/20250401225801_create_items_table.js
@@ -3,13 +3,8 @@
 exports.up = function(knex) {
     return knex.schema.createTable('items', (table) => {
       table.increments('id').primary();
-      // Define the foreign key inline for clarity:
-      table
-        .integer('user_id')
-        .unsigned()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE');
+      table.integer('user_id').unsigned();
+      table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE');
       table.string('item_name').notNullable();
       table.text('description').notNullable();
       table.integer('quantity').notNullable().defaultTo(0);
